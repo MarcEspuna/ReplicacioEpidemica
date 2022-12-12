e767ccd1f298efa0e15a3cbc58befef880dcb03a
MarcEspuna <76654758+MarcEspuna@users.noreply.github.com>
2022-12-13 00:13:55 +0100

    Delete build directory
    Deleted build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/marce/Documents/UNIVERSITAT/ProjArqDist/Sessio4
-# Build directory: C:/Users/marce/Documents/UNIVERSITAT/ProjArqDist/Sessio4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
